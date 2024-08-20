b067f32c3b3fde01df1ea58e832ae714d1682d97
Dipen <dipendraboyaju222@gmail.com>
2024-08-20 16:21:36 +0545

    f

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import expenseRoutes from './routers/expenseRoutes.js'
 dotenv.config();
 
 const app = express();
-const PORT = env.process.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors({
   // origin: 'http://localhost:5173',
