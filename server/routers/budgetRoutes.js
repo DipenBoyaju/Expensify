8dc4c2e963904146c7c924bc56a80b34291c4ddc
Dipen <dipendraboyaju222@gmail.com>
2024-08-20 22:47:07 +0545

    change

--- server/routers/budgetRoutes.js
+++ server/routers/budgetRoutes.js
@@ -5,11 +5,11 @@ import { verifyUser } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/createbudget').post(verifyUser, createBudget);
-router.route('/getbudget').get(verifyUser, getBudgets);
-router.route('/getbudget/:id').get(verifyUser, getBudgetById);
-router.route('/editbudget/:id').patch(verifyUser, editBudget);
-router.route('/deletebudget/:id').delete(verifyUser, deleteBudget);
+router.route('/createbudget').post(createBudget);
+router.route('/getbudget').get(getBudgets);
+router.route('/getbudget/:id').get(getBudgetById);
+router.route('/editbudget/:id').patch(editBudget);
+router.route('/deletebudget/:id').delete(deleteBudget);
 
 export default router;
 
